377b9d2d84e62a0d2289a2ee74f51c7642b645ca
Brandon <Brandonbretado001@gmail.com>
2023-10-11 15:08:21 -0600

    October 10

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using next_generation.Models;
+
+namespace next_generation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<NexGenUsers> GetGenUsers()
+        {
+            return new List<NexGenUsers>
+            {
+                new NexGenUsers
+{
+    Id = 1,
+    FirstName = "John",
+    LastName = "Doe",
+    Email = "john.doe@example.com",
+    CreationDate = new DateTime(2023, 5, 10),
+    Password = "password123"
+}, new NexGenUsers
+{
+    Id = 2,
+    FirstName = "Jane",
+    LastName = "Smith",
+    Email = "jane.smith@example.com",
+    CreationDate = new DateTime(2023, 6, 15),
+    Password = "securepass"
+}
+
+        };
+        }
+    }
+}
---
+++ Models/NexGenUsers.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace next_generation.Models
+{
+    public class NexGenUsers
+    {
+          public int Id { get; set; } // Primary Key
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+
+    // Validation Annotations
+    [DataType(DataType.Date)]
+    public DateTime CreationDate { get; set; }
+
+    public string Password { get; set; }
+    
+    // Other relevant properties, such as roles, preferences, etc.
+    }
+}
